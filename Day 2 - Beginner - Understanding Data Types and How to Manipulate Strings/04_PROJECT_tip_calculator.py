5037586d6655dddbf2f801badce8bb29080e6f16
not-lucky <ayshrwt801@gmail.com>
2022-03-15 22:59:12 +0530

    updated with some more bad code

--- Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/04_PROJECT_tip_calculator.py
+++ Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/04_PROJECT_tip_calculator.py
@@ -5,6 +5,6 @@ tip_percentage = float(input("What percentage tip will you like to give?\n"))
 split_among = int(input("How many people to split the bill?\n"))
 
 total_bill_with_tip = bill + (bill * tip_percentage / 100)
-each_pay = total_bill_with_tip / split_among
+each_pay = round(total_bill_with_tip / split_among, 2)
 
-print(f"Each person should pay: ${round(each_pay, 2)}")
+print(f"Each person should pay: ${each_pay:.2f}")
---
+++ Day 3 - Beginner - Control Flow and Logical Operators/06_PROJECT_treasure_island.py
@@ -0,0 +1,97 @@
+print('''@#############################################################MMMMMMMMMMMMMMMMMMBBBBMMMMBB&AMBAHM###MMMMMMM#
+@hG&&AA&&A&G&&&&GG&AA&&&&&AAAAAA&&&G&&&GGhh&AAAG93XX39h93X225iiiS5SSSSSSSiSiSSisssssiisssi:.;s;iXX25Sii25SSA
+@hAAHAAHAHAAAAAAAAAAAAAAAAAAAAAAAHHHAAAG&&3Ssi5G&GG&&GG9333X2552222222255525rrSSS55SSSiSS5X;:Xri2SX252r ;A9A
+@GAHHHHHHAAAAAAAAA&AAAAAAAAAHHHAh25iiS29B@Hr   sAX9GAAGh3333XX22222XXX5iS55Sii552225SS2X2iih&A: ,;X22r:;:ihA
+@GAAHHHHAAAAAGG&&&G&&&AA&AA&XSsii55Sirrrs&@#r   s::::r53h9XX3X252252XX2SiiiiS55525SS5irr239GGAHX39222XA3,:hA
+@3GAAAAA&AAAA&GGGGhhhGGGGG2i2hAAhir;:.    G@9   .;....:;rr53X25SS5552225rrisiSSiiiiiiir:Shh9999;iX223G&hAA9&
+@29&G933GAAAAA&Gh933393&MMM#@HXs:,:,,     ,@@,   r,     ,;;;r52SiiSiiiiisrsiissssrsssSXh399X2222222X99hGAHh&
+#5XX2X999hhGGGGGh33XXX2B#AH#MGir:::,,.     r@A   ,r        ::,:iSisssrrsssssssrrrssss5X3XX2222riX2X3333G5.;&
+#iS52XX222222X99993XX2X##323HMB9Sr;;;,      &@;   s,         ;;.:srsssrrrrrrr;r;rrsiSS5522252222X3333X3h2:rG
+#iS22255225222XXXXXXX5G@#X3#AGAB#A2rrr;,    ,@A.  ;s,        .AS ssrssrrrrrrr;rrrssiiSSiS555222sr9XXX339GA3G
+#s52222222552XXXX22225M@Ai####H3X3hAAG2i;,  ,#@r. :Sr.      ,;; ,srrr;rr::;;;rrrrssiiiiSS55222XSs22S2399332h
+#s522222255SSSS22252SX@@2;MAX29H#B&XS2HA&32ssA@X;:;Xs;::;sirr::r5rrrr;;;:::;;rrrrrsssssiSSSri3X95S3X33XXXX59
+#s5X22222255SiiiSSSSiA@@;5#Xr;;;r5&MMH@#3S5H&B@@2:,SMhSSi:i2ss;.rsrr;;;;;;;;rrrrrrsssii5252rsXXis933X22XX2i3
+#i52222222225SSiiiiis#@B:HMXs:,,,...:r2B@@##AA@A;.. @@A2Sr;,   .ssr;;;;;;;rrrrr;,,rsiSSi:rX3X23i;XX255225SsX
+#i52222222225SSiiisr5@@&r#HXr;::        .;h@@@@G:   #A:,       rSr;;;;;;rr;rrrr;..riSi,;;;2X22X9hXX2555Siir2
+#i5XXXX2222555Siiis;A@@9i#G2ir;;,.         :A#@A.   A;        .ir;;;;;;rs;:rrrrr::sSiS;;93X2X922X2225SSiiir2
+#i2XXXXXXX2255SSiirr##@3XB35Siii;:,..       h#@h;. ,B,        :r;;;;;;rrrrrrrrsi;r5iS232Sr.i2r;;25,:2, s5is2
+#SX9X33XXXXX255SisiB@##&&AX2SiiSi;;:.      ,A#@X;  ;A.      ..;r;;;;rrrrrrrrrsr;ri22222Si::X5,r5Ss:rSr;sr  X
+@23h333333XX225SirB@###AH&X2ssrrs;;:...,.  rA#@5;, X9,      . rr;:;rrrrrrrrrsisrSrr55522X5iiX32SSss5ir2srrrX
+@39hhhh9333XX25isr#@MGB#M&2irr;;;::;,,,,:,,iH@#i:, A2:,,.    .rrr;;rrrrrrrrrsriS5s;5SS5irX2525iiS5SSSSSS5X23
+@X9GGGG993XXX2Siri@##s:A@#G5sr;:,::::;:,. .5#@Mr:,,BS;;:,  .,;rrrr;;;;rrr;rssrsiS5iriSSSS55SiiiSiiiSSiiS22SX
+@XhG&&GGh93X25Sir9@#B9XAH&#@HXi;:,,,,,,,,,;G##A;:,iMrrr;:..:rirr;;;;,;rr;,r;;siiiir;iiiiiiiiiSSiiiSSSiS2XXiX
+@3h&&&GGh93X25Sir3@#HhA@M2:;B@@#AXr,.,,,, :A##3;;;AHsr;:;rr3X:r;;;;;;rr;:rrrrsrssssiiisssssiiiiiiSSSS22XX2iX
+@3h&AGhh9XXX25Sir3@##&B#AGhsi2A#@@@#Ah9&hi2##M3r;r#Mh95i9A&2 ,;;;;;;;;;;;;rsisrrrrsssrsssssissssiiSS52XX25s2
+@3GA&Gh933X225Si;&@#AAM##Gi;..,:;;rS3GH#@@@@##@MB#@MAHBH&S: :r;;;;;;:,:r;;;,..rrrrrrrrrr,;ssrrsssii552225Sr5
+@3GGGGh993X22SSi;X@MGAAXG#BS:.            :9M2:,,2@r.;;,    ;;;::::;,:;;;;:   ,:;;;;;;r;;rr;rrrsiiiS55SSSirS
+@3hhhhh93XX25Sis;&@MBBAr.rH@#hr,          i@#X,  :Mr.      :;:,:::::,:;;;;;;rr:,:,;;;;;;r;;,;rrrsssiiiiiis;S
+@X39h9933X22SSisrM@MAH#M9iX@@@@B5r:,     .i@Hr:  rs    .;: ;;s222srrr:,,,::::;:,:::,;. :;;;;;;;;rrrsrrrsrr;i
+@X99993XX225SSiri@#AXAMM#BXh@@@@@@@@#BGX2SAM#&;rXG;:SB@@@sr2M@@#2:sXAHhi;;;;r. ;;::,,,,::,,:;iS23h22s;rr;;:s
+@X993XXX225SSisr3@#BAHBXSX2;;h##@@@@@@@@@@#M@#X&@@@@@@@H. ;G@MB&;;2isS9HMHA3i:;XHMXis,,:,;S&&3##H2;:,:::,::s
+@X393X2225Sii35s&###BMM3sr;r5H#&ii5GMBAM@@@#@@H@@@Hr. ;i  r##GGr ;srr;;,, :. :rsXM92@HGA#@@2 .hH&G&hX5i593SS
+@2X3XX225Sii2#@@#HH###Mh2i;,;HAs:.   ,,,:5ABX;,.Xi     2 2@A&G3Srrrr:::,,:::,,;iAAM@@@@@#HH3iGG99h&H##AX;, r
+@2XXX22SS2A#@@@@##M###M&hhiri#&:.,      :A@Bi:  ;r    i:5@B3sX25ir;;:,,,,,,,:.:sS&h#@@#MHA&AHGXXX3hhG&3srr;s
+@2XX255XhM@@@@#######@@#HAhX&@Hr:;:     .H@X:is;r:.  :S5@MGr.:;;:,:r;;r;,. .:. s2H@#@@MA&G&&Ss33333h99G&GH#A
+@2Xh93MBA@@@#######BB#@@@BAAM#AXi;rr.   .A@9:,::r:.  S3#AX;,.. .,,.:s;,:,, .rs:2A@@Mr;GAGh9h;:3XXi.S33GAA&3&
+@X2Si29A@@########B95XB@@@BBB#B&&Srs;  .S#@MA2;rr.  :MAGs:,.,,,,::, ,:.:r5r;9A2XM@#&..&BAGG&A&933X2X3X9hAHHH
+#3h2SisSB#BBM##M###A52M#M@##MH&3Xir;:,,;X@#i;;:S;  .HH5i, .,:,::,;, .;;s2939#H9A@@#@@@BG9X999333339X222X9hGH
+#2M###A9XBMM#BHBMMMAA##AXG@###ASr;;;:,:;3#3;:,;G;,:AArrr..::;,;:.:;,rh5s9AAM#A&##HHHHAh3XXX3GG3393X2X3X2252A
+#sA#M#@#AA#@#&AHAHHH##HA5.:M#3A@hr;;::,:9MAi5,S2:;Ghr;s;,:;:;:;;;;rr3hiiAHAAAXA#MBHAAh9hhGGs :XG9339Gh3XX99A
+#iGA2hHMBHHAGGAAGGH#@BAB3  i@@r:M@9r::,:9#B:,:&r;A2s;rssr;;;::srrss5hXiXABBBHB@MHA&AA&&&&&&i  iAhGAAG3XXX39A
+@2AHir59MB5rsXAMBAM#AGGAX:;Ar.s, rGM9r:sX#G;.2@BGX;r,;i32iir;r5r5X2AAS2#@@####MMBAGGGX23X9&AMHA&&GhhX22XGAAB
+@GHAirShHASi2&&AAAAA99hXsiMG  .9#G&@@@#@##B2r@#Ss;;: ,iXXXh2rhh2&GAH2SM@@###MHAAAAhh99h&HiiBHHA&Ghhh9GAAHAAB
+@AAA9GHAHMHBBAhGA&H@BGHHA@@5SB@@@##MBMMBHAB&Xr,,,,;,.rXrShh5S9SX3G&s5@@@#MBHHHAA&GAAAAHAA99BAA&&GGGh3hGAAAAH
+@9&HMBBAMBMBAhAA93&BHGGhAB#M@@M35sr;;:;rSXX;.  ,.,;:;X&S9XsrsriA@A;s@@#MMMBHHHHBh;X&&&AX;iHG&&&GhhGGAAAAAHHM
+@9AMMMBAhX523&AXS2hhXXhhXX9GGh5srr;;::;i93r:.:2SrS35iA&GAi;r5SH#5:i@@#MBBB#MBHBBM9BBA22BMBAGG&AAHMBAAG9hh&AB
+@GGAA32sr;;sSii2XG9irS5522X3XX99hGA&&9hAhS;:;X9siA&52A222Si3AA2;i33@#ABMBH&GHHX3BHM##MX9&AHBHHB#@G3HH&G93X2A
+@#H&h333335iiiXX3S;;ihSsi5X9hhh&A&G9GGh2sr;rAA22h5:;3HA3i3MBAX9@@@##BBM2hMS9HHSriAsGBM#BAGAHMM#2   .GAHAAGXA
+@BMMM#MAAA9Xh&3h2si5XG5sr;;rsiisssiSirrrsrXBA9GA5::2H9&A2SXG#@@@@@##MBM55MHM##@@#@#G&AHHAAAAB@s     G#AHAHBB
+@&ABB#####HH&X399G3X22iS5Sr:;;::sSSs;ri52GH&h3X2SXG95r:,sA..:s9A#@@#@MM@@M###@H.  2@MAAHHHB##   :;sA@@#BHA&B
+@&A&AHBM#@@@@MHAGGh3222239X25ShMX552hHHB#@#&GAHMBG5r:,s#@X.:;;;2@@BB##@####MM@     S@@#####@B    ,X@@##MMBAB
+@A&HBHMMBH##@@#MMM##@@@A9A#HG@@@@GAHH#@#&X5SsiXS;,  :B@@r .... r@G;H@####BMB#@: ,rr;2@@@@@@@@2;sH@#@@MMMBBAB
+@AB#HAHBBH2G2S5X3933hAMMAGX5s#@@@s;rr;::...       .9@@X            S#@#####M#@@  ,rs.r@@@@#2s:rG@@#@##HBBBAB
+@HMM359GAirs;;rr;;;;:::::,,, G@@@:             ,2H@@M.           :, ;H@@#####@@@  ;Si;X; s.     i@#&B@MHHHAM
+@ABX2@&s;:;;rr;::,,.         #BA#,           2@@@BBB       .  :rrSir;:9@@#####@@@..;;2r   i   ;2M@#H#MHHHHAB
+@&Xrr9@@B2r;,.,...          r@93M;         ;#@#BBXr               ....:H#5M###@@2.;;.;r.  S#AGH@@MHHM@#BAHAB
+@r;sirS@@@@@&:  ...   ..... ,@AA#H        h@@BA#s                  .,, ;Hi#@@@3      r92:GG@@@@@@#&M#HBMMA&B
+#r532i;:A@@@@@5 .,:::;;:,.. 5@HH#@,     ,HB&AA#2                     ..,r.  2@.    . ;A#HB@@@@@@@M@B##MAGHBB
+#S22i;;,.:i@@@@@r,          ,@#M#@@    5@@AhAA:                      ...     Xr    :, 3M3s@@@@#M@#@BH#HH#MhH
+#iiirrr;:.  2@@@@@&s:   ..   B@#@@@; .B@Bh9A9                        ,,r..r. ,A;  .r. ,2AAM@@@@@@M@AA99MAMHM
+#sssssrr;:,.  3@@@@@@@;     5@3rrsX#@@@A3r2&                .,    ...,r:rh,   XA;:,.   r&MM@@#@@H#A&H929G#AB
+@iSisrrr;;;,.  r@@@@@@@@&Sh@B,    ..:s&#B&M                 s&;  .:r;2,  i2rsX; ,5;2hs;XGH#@@@@MM#BB#9hhX&5A
+#iSisrrrr;;::,   ,r9@@@@@@@2.,;;;;rr:..i#@5           .  ..  :XBX;  :;.   ;Air;,s:,H@@@M#h 2@@@@@#A@B&#3&3SA
+#iiirrr;;r;;:,       ,A@@@M;5X222H@B3ir:3@#2AHHAA&GG9#r .  .9hA@@#9s  ;r.  .2,r@@A5X3@@#@#.  5@@@@@@@@@#HAXA
+@SSiisisr;;:,..        :#@#H@@BHHA@@@@@, M@@@@@@@@@@@@#&i  :;2@@#B@#X;  ,   ,sH#A#@#XG##AB@X    .;Sh&@@@@B&A
+@XXXXXSr;:::,,:::::,,:;rH@M@@@#35A&s29i,,@@#AM@@@@@@@@@@@@@5   .  .#@@s , ,. .H@@HhX3HMH3S5#@B;        s@@#H
+@&&h2s;;;;;;;;r;;r2H@@@@@@XhHMhGA@2;59GA9@@@MGSr;:,,,,,,r#@@#s: ,   r@@#@53:s,A@@@#AAHHhXi:,s@@#r        2@#
+@XXSrr;;;rrsrrS&@@@@@@@@@@M&AHB#@#&hGG92r2@s               9@@@Ar .  .A@@@@#@###M###MBAh99i;.,5@@X:,   :. :@
+@iissiissiS3M@@@@@@@@@@@#@@G&hG#MMHA2r;:,h#B5                S@@@#&:rAB@@@@@@@##A&&ABMHh3GGS;. .3@B;:;, sA,X
+@issS252&#@@@@@@@@#H3Sr.,#@2isssi2;::,.:h#AH@@i                ,9@@@@@@@@@@@@@#A3X3&H#MHAGh2s;.  ,9#5,;;.;AG
+#rs552B@@@@@@B9Sr;:..  ;@@@#Srr;:,,,.,&HBA59H#@B.                 .2BX,   ,2AB##&iiX39HMHHh32r:.   .9#r.,:5h
+#59A@@@@@B3ir;:::,:::,i@@@@@@@GSsrrs3@9  . 5#MM@@i                           .iH@AsrssSAHA#A&2;,     ,AG;2;r
+@h#@@#&2isrr;;;:;;;r;r@@@@@@XA@@@@@@@,     .i@@#@@#:                  ..,.,::..rh@#XsrrSAAG@BAs:,      r@@ :
+@AMHhX2Ssrrrrrrssiii9@@@@@@r  :@@@@@A  ..    .M@@@@@&;                 .,,;;rrr;S5##S;;riA3X@#As:,     :@2 .
+@AA&32irrrrsi5222iS#@@@@@@r ,..@@@#@A ...      ;hM@@@@@X                    :r25is;##, :;sBS2@#ASr:::,:h&  :
+@GG32Siiii52X22i;2@@@@@@9. ..  ##AA#5              r@@@@@;   ...       .... ,;s5X:;;#A  .;s#i5#HG2iss2M&.  :
+@9hh3XX39h925Ss;A@@@@@X.   ..  S@BH#2                A@#@@h      ......  ..:;rrsX: r:#A   ,;#r2#AAAM@@2: . i
+@&HBHHHAGXSsssr#@@@@h,  ,:::;:..@@@@@ .,,,,.....      ;h@@@@,        .:::....;siri, ;,#H,   :M;A#B#@3;;:r,;@
+@A##BHAGX55SSr2@@@A;,:riiSiiisr,r@@@@i,;;;;:;;,,:;::,   .s#@@A         .:;rr:,:;;i@,ii,MHr,:rB#i#@9r:,.r,;@@
+@A##BA&9X2225rB@@G:ri522222255ir:@@@@3;s;ssrr;,.,,,.        i@@2,...     .,;iSi;:r@@@@S,&h&@@#@##M2i  ,:X@@#
+@&HMHG3Xisiir#@&s:;rssrrr;::;r;;,&@@@G.:,,..                   S;.,,:,,.    ,r3&3rB@#5@h;rs9@#&32A#HSsG@@@XH
+@&AB&3X2Sssri@s .,:::::::,  ,,,. 5@MG.        .,,::,:::::,..         .,,::::::;i2X@@#;s@#XS::53XS2M@@@@@MMAA
+@AHA&G325Sisir,:::,,,,,..         @@9     ,;;;rsis;rr;;::,...........   .:rirr;rr;9@@@H#@@@MAM@@@@@@@@@@@MB#
+@h3XXX333X2ir;;;;;;;;;:,,,,,,...  A@@,;rsiiSSiiiir..:,..             ..,:;;;;rsiri&###@#@@@@@@@@@@@@@2r293iH
+#255issiiSSSiisr;;;;::;,:;;;;rrrrriBHsssi;,;rrrr;;;:::,,,..            .,::;;rrrs2BMBMMMMBMM#@@@@@@@#9XS292A''')
+
+print('''The way I see it, our fates appear to be intertwined.
+In a land brimming with Hollows, could that really be mere chance?
+So, what do you say? Why not help one another on this lonely journey?''')
+
+choice = input('Choose fellow warrior.\nYes or No?\n').lower()
+
+if choice == 'yes':
+    print('Lets get going then.')
+    print('I am bored now, so bye lul.')
+else:
+    print('Ahh warrior, so you decided to continue your own adventure. \
+    Then I shall not bother you for I too have to seek my own sun')
