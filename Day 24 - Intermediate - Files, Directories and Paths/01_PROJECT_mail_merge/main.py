e75092fd7bb582fa3d74d46e5c01fd10221aedf2
not-lucky <57046712+not-lucky@users.noreply.github.com>
2022-03-22 20:05:11 +0530

    updated with some more bad code

--- Day 24 - Intermediate - Files, Directories and Paths/01_PROJECT_mail_merge/main.py
+++ Day 24 - Intermediate - Files, Directories and Paths/01_PROJECT_mail_merge/main.py
@@ -1,4 +1,4 @@
-#TODO: Create a letter using starting_letter.txt 
+'''#TODO: Create a letter using starting_letter.txt 
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
@@ -6,3 +6,14 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+'''
+
+with open('Input/Names/invited_names.txt') as fl:
+    names = fl.read().splitlines()
+
+with open('Input/Letters/starting_letter.txt') as fl:
+    text = fl.read()
+
+for name in names:
+    with open(f'Output/ReadyToSend/letter_for_{name}.txt', 'w') as nm:
+        print(text.replace('[name]', name), file=nm)
